0aab7dae62fa40d90c145cb0937b24ee438cd317
FlysongMH <song.m.h@163.com>
2020-02-26 07:46:00 +0800

    PyQt5使用APP主题设置；qDebug导入

--- PyQt5Project/PivotTable/main.py
+++ PyQt5Project/PivotTable/main.py
@@ -1,9 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import * # PyQt5中的qDebug在此
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 from PivotTable import PivotWindow
 
 
 if __name__ == '__main__':
+    # APP settings
     app = QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create('Fusion'))  # <- Choose the style
+
+    # mainWindow settings
     win = PivotWindow()
+
+    # system settings
     sys.exit(app.exec_())
