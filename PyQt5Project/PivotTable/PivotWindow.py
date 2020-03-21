d8def999627f89c413e39ae68963450ac4f4ab67
FlysongMH <song.m.h@163.com>
2020-03-21 18:24:06 +0800

    为导入的PandasModel绘制图形，使用QChart

--- PyQt5Project/PivotTable/PivotWindow.py
+++ PyQt5Project/PivotTable/PivotWindow.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QLabel, QFileDialog
+from PyQt5 import QtChart
 from ui_PivotWindow import Ui_Pivot
 from table_model import PandasModel
 
@@ -19,21 +20,24 @@ class PivotWindow(QMainWindow):
         # ui初始化, 新建一个成员ui
         self.ui = Ui_Pivot()
         self.ui.setupUi(self)
-
-        # 窗口居中
-        qr = self.frameGeometry()  # 获得主窗口所在的框架。
-        cp = QDesktopWidget().availableGeometry().center()  # 获取显示器的分辨率，然后得到屏幕中间点的位置。
-        qr.moveCenter(cp)  # 把主窗口框架的中心点放置到屏幕的中心位置。
-        self.move(qr.topLeft())  # 通过move函数把主窗口的左上角移动到其框架的左上角，这样就把窗口居中了。
+        self.ui_moveCenter(self)
 
     def init_var(self):
         self.cwd = os.getcwd()
+        self.data_isValid = False
         self.dataList = []
 
     def init_slot(self):
         # signal and slot
         self.ui.actiontool_label.triggered.connect(self.slot_show_label)
         self.ui.actiontool_import.triggered.connect(self.slot_import_data)
+
+    def ui_moveCenter(self, window):
+        # 窗口居中
+        qr = window.frameGeometry()  # 获得主窗口所在的框架。
+        cp = QDesktopWidget().availableGeometry().center()  # 获取显示器的分辨率，然后得到屏幕中间点的位置。
+        qr.moveCenter(cp)  # 把主窗口框架的中心点放置到屏幕的中心位置。
+        window.move(qr.topLeft())  # 通过move函数把主窗口的左上角移动到其框架的左上角，这样就把窗口居中了。
 
     def slot_show_label(self):
         self.label = QLabel("This is QLabel")
@@ -44,20 +48,75 @@ class PivotWindow(QMainWindow):
         files, filetype = QFileDialog.getOpenFileNames(self, "多文件选择", self.cwd,
                                                        "All Files (*);;csv Files (*.csv);;Text Files (*.txt)")
         if len(files) == 0:
-            print("\n您已取消选择")
+            print("\n您已取消选择.")
             return
         print("\n你选择的文件为:")
         for file in files:
             print(file)
         print("文件筛选器类型：", filetype)
-
+        # files=[r"F:\Qt5Project\MyQtProject\PyQt5Project\PivotTable\TestData.csv"]
         # 导入数据
         for file in files:
             if (file.endswith(".csv")):
                 temp = pd.read_csv(file)
-                print(temp.shape)
+                print('data.shape:', temp.shape)
+                print('data.index:', temp.index)
+                print('data.columns:', temp.columns)
                 self.dataList.append(temp)
+                self.data_isValid = True
                 break
+        self.reset_model()
 
+    def reset_model(self):
         self.model = PandasModel(self.dataList[0])
         self.ui.tableView.setModel(self.model)
+        print('reset model success!')
+        self.show_chart(col='depth')
+
+    def show_chart(self, col):
+        '''
+        1.QChartView : 是一个可以显示图表（chart）的独立部件。
+        2.QChart : 用于创建图表。
+        3.series : 被词典翻译为 串联;级数;系列，连续; 这里我理解为存储的一系列坐标；
+        4.QValueAxis : 继承自QAbstractAxis,用于对坐标轴进行操作。
+        流程: 坐标附加到series然后使用addSeries方法把series载入ChartView。
+        '''
+        if not self.data_isValid:
+            return
+
+        # creating LineSeries
+        data = self.dataList[0]
+        len = data.shape[0]
+        self.series = QtChart.QLineSeries()
+        self.series.setName(col)
+        for idx in data.index:
+            self.series.append(idx, data.loc[idx,col])
+        print(data.index.min(), data.index.max())
+
+        # define x/y axis
+        self.x_axis = QtChart.QValueAxis()
+        self.x_axis.setRange(data.index.min(), data.index.max())
+        self.x_axis.setLabelFormat("%0.1f")  # 设置坐标轴的显示方式，精确到小数点后1位
+        self.x_axis.setTickCount(len)
+        self.x_axis.setMinorTickCount(1)  # 设置每个单元格有几个小的分级
+
+        self.y_axis = QtChart.QValueAxis()
+        self.y_axis.setRange(data[col].min(), data[col].max())
+        self.y_axis.setLabelFormat("%0.2f")  # 设置坐标轴的显示方式，精确到小数点后2位
+        self.y_axis.setTickCount(10)
+        self.y_axis.setMinorTickCount(0)  # 设置每个单元格有几个小的分级
+
+        # Creating QChartQview
+        self.chartView = QtChart.QChartView()
+        self.chartView.resize(800,600)
+        self.ui_moveCenter(self.chartView)
+        self.chartView.chart().addSeries(self.series)
+        self.chartView.chart().setAxisX(self.x_axis)  # 设置x轴属性
+        self.chartView.chart().setAxisY(self.y_axis)  # 设置y轴属性
+        # self.chartView.chart().createDefaultAxes() # 使用默认坐标系
+        self.chartView.chart().setTitle("数据列显示：%s" % col)  # 设置标题
+        self.chartView.show()
+
+
+
+
