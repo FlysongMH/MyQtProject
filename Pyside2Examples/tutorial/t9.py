0c5928d0dd4499da6abd86e364c321cff45915f8
FlysongMH <song.m.h@163.com>
2020-03-22 20:54:47 +0800

    # PySide2 tutorial 9 # 关于画布的一些用法，可以通过QtGui.QPainter进行组合绘制复杂的图形

--- Pyside2Examples/tutorial/t9.py
+++ Pyside2Examples/tutorial/t9.py
@@ -1,46 +1,5 @@
-
-#############################################################################
-##
-## Copyright (C) 2016 The Qt Company Ltd.
-## Contact: http://www.qt.io/licensing/
-##
-## This file is part of the Qt for Python examples of the Qt Toolkit.
-##
-## $QT_BEGIN_LICENSE:BSD$
-## You may use this file under the terms of the BSD license as follows:
-##
-## "Redistribution and use in source and binary forms, with or without
-## modification, are permitted provided that the following conditions are
-## met:
-##   * Redistributions of source code must retain the above copyright
-##     notice, this list of conditions and the following disclaimer.
-##   * Redistributions in binary form must reproduce the above copyright
-##     notice, this list of conditions and the following disclaimer in
-##     the documentation and/or other materials provided with the
-##     distribution.
-##   * Neither the name of The Qt Company Ltd nor the names of its
-##     contributors may be used to endorse or promote products derived
-##     from this software without specific prior written permission.
-##
-##
-## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-##
-## $QT_END_LICENSE$
-##
-#############################################################################
-
 # PySide2 tutorial 9
-
+# 关于画布的一些用法，可以通过QtGui.QPainter进行组合绘制复杂的图形
 
 import sys
 from PySide2 import QtCore, QtGui, QtWidgets
@@ -115,10 +74,11 @@ class CannonField(QtWidgets.QWidget):
         painter.setPen(QtCore.Qt.NoPen)
         painter.setBrush(QtCore.Qt.blue)
 
-        painter.translate(0, self.rect().height())
-        painter.drawPie(QtCore.QRect(-35, -35, 70, 70), 0, 90 * 16)
-        painter.rotate(-self.currentAngle)
-        painter.drawRect(QtCore.QRect(33, -4, 15, 8))
+        h = self.rect().height()
+        painter.translate(0, self.rect().height())  # 坐标转换：中心坐标平移位置. 左为x，下为y # 此处为移到左下角
+        painter.drawPie(QtCore.QRect(-h/2, -h/2, h, h), 0*16, 90*16)  # 绘制饼型和扇形 # 注意这里的角度单位是1/16度
+        painter.rotate(-self.currentAngle)  # 整个画布进行了逆时针旋转
+        painter.drawRect(QtCore.QRect(0, -2, h, 4))  # 绘制一个方框
 
 
 class MyWidget(QtWidgets.QWidget):
