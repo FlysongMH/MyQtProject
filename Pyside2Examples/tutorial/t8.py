823301f72199287ec88a207dd800ec971cc530ce
FlysongMH <song.m.h@163.com>
2020-03-22 20:19:40 +0800

    # PySide2 tutorial 8 # Widget的绘图功能示例。更方便的绘图可以考虑使用QtChart

--- Pyside2Examples/tutorial/t8.py
+++ Pyside2Examples/tutorial/t8.py
@@ -1,72 +1,34 @@
-
-#############################################################################
-##
-## Copyright (C) 2016 The Qt Company Ltd.
-## Contact: http://www.qt.io/licensing/
-##
-## This file is part of the Qt for Python examples of the Qt Toolkit.
-##
-## $QT_BEGIN_LICENSE:BSD$
-## You may use this file under the terms of the BSD license as follows:
-##
-## "Redistribution and use in source and binary forms, with or without
-## modification, are permitted provided that the following conditions are
-## met:
-##   * Redistributions of source code must retain the above copyright
-##     notice, this list of conditions and the following disclaimer.
-##   * Redistributions in binary form must reproduce the above copyright
-##     notice, this list of conditions and the following disclaimer in
-##     the documentation and/or other materials provided with the
-##     distribution.
-##   * Neither the name of The Qt Company Ltd nor the names of its
-##     contributors may be used to endorse or promote products derived
-##     from this software without specific prior written permission.
-##
-##
-## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-##
-## $QT_END_LICENSE$
-##
-#############################################################################
-
 # PySide2 tutorial 8
-
+# Widget的绘图功能示例。更方便的绘图可以考虑使用QtChart
+# 调色板 https://doc.qt.io/qtforpython/PySide2/QtGui/QPalette.html
 
 import sys
 from PySide2 import QtCore, QtGui, QtWidgets
 
 
 class LCDRange(QtWidgets.QWidget):
-    valueChanged = QtCore.Signal(int)
+    valueChanged = QtCore.Signal(int)  # Signal
+
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
 
         lcd = QtWidgets.QLCDNumber(2)
+
         self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         self.slider.setRange(0, 99)
         self.slider.setValue(0)
 
-        self.connect(self.slider, QtCore.SIGNAL("valueChanged(int)"),
-                     lcd, QtCore.SLOT("display(int)"))
-        self.connect(self.slider, QtCore.SIGNAL("valueChanged(int)"),
-                     self, QtCore.SIGNAL("valueChanged(int)"))
+        # self.connect(self.slider, QtCore.SIGNAL("valueChanged(int)"), lcd, QtCore.SLOT("display(int)"))
+        # self.connect(self.slider, QtCore.SIGNAL("valueChanged(int)"), self, QtCore.SIGNAL("valueChanged(int)"))
+        self.slider.valueChanged.connect(lcd.display)
+        self.slider.valueChanged.connect(self.valueChanged)
 
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(lcd)
         layout.addWidget(self.slider)
         self.setLayout(layout)
 
-        self.setFocusProxy(self.slider)
+        self.setFocusProxy(self.slider)  # Focus
 
     def value(self):
         return self.slider.value()
@@ -84,14 +46,15 @@ class LCDRange(QtWidgets.QWidget):
 
         self.slider.setRange(minValue, maxValue)
 
-
+# Widget的绘图功能示例
 class CannonField(QtWidgets.QWidget):
-    angleChanged = QtCore.Signal(int)
+    angleChanged = QtCore.Signal(int)  # Signal
+
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
 
         self.currentAngle = 45
-        self.setPalette(QtGui.QPalette(QtGui.QColor(250, 250, 200)))
+        self.setPalette(QtGui.QPalette(QtGui.QColor(250, 250, 200)))  # 调色板
         self.setAutoFillBackground(True)
 
     def angle(self):
@@ -107,12 +70,14 @@ class CannonField(QtWidgets.QWidget):
             return
         self.currentAngle = angle
         self.update()
-        self.emit(QtCore.SIGNAL("angleChanged(int)"), self.currentAngle)
+        # self.emit(QtCore.SIGNAL("angleChanged(int)"), self.currentAngle)
+        self.angleChanged.emit(self.currentAngle)  # 发送信号的新式写法: 对象.信号.emit(信号入参值)
 
+    # 这是重新实现的绘图事件, QtWidgets.QWidget.paintEvent
     def paintEvent(self, event):
-        painter = QtGui.QPainter(self)
-        painter.drawText(200, 200, "Angle = %d" % self.currentAngle)
-
+        painter = QtGui.QPainter(self)  # painter 画笔
+        painter.setFont(QtGui.QFont('微软雅黑',40))  # 设置画笔字体大小
+        painter.drawText(200, 200, "Angle = %d" % self.currentAngle)  # 在给定位置输出文本，如何设置显示大小？
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self, parent=None):
@@ -121,23 +86,23 @@ class MyWidget(QtWidgets.QWidget):
         quit = QtWidgets.QPushButton("Quit")
         quit.setFont(QtGui.QFont("Times", 18, QtGui.QFont.Bold))
 
-        self.connect(quit, QtCore.SIGNAL("clicked()"),
-                     QtWidgets.qApp, QtCore.SLOT("quit()"))
+        # self.connect(quit, QtCore.SIGNAL("clicked()"), QtWidgets.qApp, QtCore.SLOT("quit()"))
+        quit.clicked.connect(QtWidgets.QApplication.quit)
 
         angle = LCDRange()
         angle.setRange(5, 70)
 
         cannonField = CannonField()
 
-        self.connect(angle, QtCore.SIGNAL("valueChanged(int)"),
-                     cannonField.setAngle)
-        self.connect(cannonField, QtCore.SIGNAL("angleChanged(int)"),
-                     angle.setValue)
+        # self.connect(angle, QtCore.SIGNAL("valueChanged(int)"), cannonField.setAngle)
+        # self.connect(cannonField, QtCore.SIGNAL("angleChanged(int)"), angle.setValue)
+        angle.valueChanged.connect(cannonField.setAngle)
+        cannonField.angleChanged.connect(angle.setValue)
 
         gridLayout = QtWidgets.QGridLayout()
         gridLayout.addWidget(quit, 0, 0)
         gridLayout.addWidget(angle, 1, 0)
-        gridLayout.addWidget(cannonField, 1, 1, 2, 1)
+        gridLayout.addWidget(cannonField, 0, 1, 3, 1)  # 在网格中占了多行，表示从(0,1)->(3,1)的区域
         gridLayout.setColumnStretch(1, 10)
         self.setLayout(gridLayout)
 
@@ -148,5 +113,6 @@ class MyWidget(QtWidgets.QWidget):
 app = QtWidgets.QApplication(sys.argv)
 widget = MyWidget()
 widget.setGeometry(100, 100, 500, 355)
+widget.resize(800,600)
 widget.show()
 sys.exit(app.exec_())
