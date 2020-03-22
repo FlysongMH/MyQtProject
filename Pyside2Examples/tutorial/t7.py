a41c28c0ce49b44b34087f7562ee071f987cfcf8
FlysongMH <song.m.h@163.com>
2020-03-22 19:44:07 +0800

    # PySide2 tutorial 7: # 自定义信号的处理

--- Pyside2Examples/tutorial/t7.py
+++ Pyside2Examples/tutorial/t7.py
@@ -1,53 +1,18 @@
-
-#############################################################################
-##
-## Copyright (C) 2016 The Qt Company Ltd.
-## Contact: http://www.qt.io/licensing/
-##
-## This file is part of the Qt for Python examples of the Qt Toolkit.
-##
-## $QT_BEGIN_LICENSE:BSD$
-## You may use this file under the terms of the BSD license as follows:
-##
-## "Redistribution and use in source and binary forms, with or without
-## modification, are permitted provided that the following conditions are
-## met:
-##   * Redistributions of source code must retain the above copyright
-##     notice, this list of conditions and the following disclaimer.
-##   * Redistributions in binary form must reproduce the above copyright
-##     notice, this list of conditions and the following disclaimer in
-##     the documentation and/or other materials provided with the
-##     distribution.
-##   * Neither the name of The Qt Company Ltd nor the names of its
-##     contributors may be used to endorse or promote products derived
-##     from this software without specific prior written permission.
-##
-##
-## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-## "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-## LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-## A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-## OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-## SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-## LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-## DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-## THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-## OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-##
-## $QT_END_LICENSE$
-##
-#############################################################################
-
 # PySide2 tutorial 7
-
-
+# 自定义信号的处理
+# 每个LCDRange类自定义了一个valueChanged信号函数，用于表示值变更的信号，当成员slider的值改变时触发信号
+# MyWidget将9个LCDRange做了联动处理，当一个LCDRange变更会设置前面LCDRange的值，从而达成联动修改的效果。
+# PyQt5中对应@QtCore.Slot(int)的是@QtCore.pyqtSignal(int)
 import sys
 from PySide2 import QtCore, QtGui, QtWidgets
 
 
 class LCDRange(QtWidgets.QWidget):
-    valueChanged = QtCore.Signal(int)
+    # 自定义信号,信号类型为int,其中类型与python的类型相对应,比如list传递一个列表，注意与def是一个级别的
+    # 信号实际就做了一件事，出发slot，并将入参(若有)传递给slot
+    # 此处信号并没有执行emit(), 是否可行?
+    valueChanged = QtCore.Signal(int)  # 可以用其他名字与slider的valueChanged区分开
+
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
 
@@ -57,10 +22,8 @@ class LCDRange(QtWidgets.QWidget):
         self.slider.setRange(0, 99)
         self.slider.setValue(0)
 
-        self.connect(self.slider, QtCore.SIGNAL("valueChanged(int)"),
-                     lcd, QtCore.SLOT("display(int)"))
-        self.connect(self.slider, QtCore.SIGNAL("valueChanged(int)"),
-                     self, QtCore.SIGNAL("valueChanged(int)"))
+        self.slider.valueChanged.connect(lcd.display)
+        self.slider.valueChanged.connect(self.valueChanged)
 
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(lcd)
@@ -70,7 +33,7 @@ class LCDRange(QtWidgets.QWidget):
     def value(self):
         return self.slider.value()
 
-    @QtCore.Slot(int)
+    # @QtCore.Slot(int)
     def setValue(self, value):
         self.slider.setValue(value)
 
@@ -82,12 +45,10 @@ class MyWidget(QtWidgets.QWidget):
         quit = QtWidgets.QPushButton("Quit")
         quit.setFont(QtGui.QFont("Times", 18, QtGui.QFont.Bold))
 
-        self.connect(quit, QtCore.SIGNAL("clicked()"),
-                     QtWidgets.qApp, QtCore.SLOT("quit()"))
+        quit.clicked.connect(QtWidgets.QApplication.quit)
 
         grid = QtWidgets.QGridLayout()
         previousRange = None
-
 
         layout = QtWidgets.QVBoxLayout()
         layout.addWidget(quit)
@@ -100,8 +61,8 @@ class MyWidget(QtWidgets.QWidget):
                 grid.addWidget(lcdRange, row, column)
 
                 if previousRange:
-                    self.connect(lcdRange, QtCore.SIGNAL("valueChanged(int)"),
-                                 previousRange.setValue)
+                    # self.connect(lcdRange, QtCore.SIGNAL("valueChanged(int)"), previousRange.setValue)
+                    lcdRange.valueChanged.connect(previousRange.setValue)
 
                 previousRange = lcdRange
 
