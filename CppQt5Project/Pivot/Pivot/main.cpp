de77eb1ad70d90ef6bddd99856f932ac8e03475a
FlysongMH <song.m.h@163.com>
2020-08-01 16:29:43 +0800

    设置APP主题Fusion

--- CppQt5Project/Pivot/Pivot/main.cpp
+++ CppQt5Project/Pivot/Pivot/main.cpp
@@ -1,12 +1,16 @@
 ﻿#include "pivot.h"
+#include <QDebug>
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+    //qDebug()<<QStyleFactory::keys(); //("Windows", "WindowsXP", "WindowsVista", "Fusion") //QT内置样式
+    app.setStyle(QStyleFactory::create("Fusion"));
     Pivot w;
     w.setWindowIcon(QIcon(":/ico/logo.ico")); //设置窗口图标
     w.show();
     
-    return a.exec();
+    return app.exec();
 }
